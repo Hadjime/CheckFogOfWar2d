c4caf22036bd22edb403da371873ad0faaa21a54
Hadjime <GavrilovRoman@inbox.ru>
2020-02-20 22:45:42 +0700

    Попытка через шейдер

---
+++ Assets/Scripts/CameraMoveToPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveToPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    private Vector3 currentPosition;
+    void LateUpdate()
+    {
+        var position = player.position;
+        currentPosition.Set(position.x, position.y, -10);
+        transform.position = currentPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    public InputAction actionMove;
+
+    private Vector3 direction;
+    private float speed = 2f;
+    private void Start()
+    {
+        actionMove.performed += HandlerMove;
+    }
+
+    private void HandlerMove(InputAction.CallbackContext context)
+    {
+        direction = new Vector3(context.ReadValue<Vector2>().x, context.ReadValue<Vector2>().y, 0); 
+    }
+
+    private void OnEnable()
+    {
+        actionMove.Enable();
+    }
+    private void OnDisable()
+    {
+        actionMove.Disable();
+    }
+
+    private void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+}
